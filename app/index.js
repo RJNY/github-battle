77632aa384c685c500c479c2f6133781904ec1cb
Richard Macias <rjny86@gmail.com>
2016-11-11 00:08:30 -0500

    react components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,46 @@
+var USER_DATA = {
+  name: "Richard Macias",
+  username: "RJNY",
+  image: "https://s.gravatar.com/avatar/23ed822c0798e344e9f8866a174fe1bf?s=80"
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function(){
-    return (
-      <div> Hello ReactJS Program </div>
-    )
+    return <img src={this.props.imageUrl} />
   }
 });
 
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('app')
-);
+var ProfileLink = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render( <Avatar user={USER_DATA}/>, document.getElementById('app'));
